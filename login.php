f750bfdbb9248e39d1c522428bd65c53794df3aa
newmax2025 <newmaxconsultas@gmail.com>
2025-03-24 22:31:38 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,41 +1,19 @@
 <?php
-require 'config.php'; // Conexão com o banco
+require 'config.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $usuario = $_POST['username'];
     $senha = $_POST['password'];
-    $captchaResponse = $_POST['cf-turnstile-response'];
 
-    //Verifica se o CAPTCHA foi preenchido
-    if (empty($captchaResponse)) {
-        echo "Erro: CAPTCHA obrigatório!";
-        exit;
-    }
-
-    //Busca o usuário no banco
-    $sql = "SELECT * FROM clientes WHERE usuario = :usuario";
+    // Busca o usuário no banco
+    $sql = "SELECT * FROM clientes WHERE usuario = :usuario AND senha = :senha";
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':usuario', $usuario);
+    $stmt->bindParam(':senha', $senha);
     $stmt->execute();
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
     if ($user) {
-    echo "Usuário encontrado no banco!<br>";
-    echo "Senha digitada: " . $senha . "<br>";
-    echo "Senha no banco: " . $user['senha'] . "<br>";
-
-    if (password_verify($senha, $user['senha'])) {
-        echo "Senha correta!";
-    } else {
-        echo "Senha incorreta!";
-    }
-} else {
-    echo "Usuário não encontrado!";
-}
-exit;
-
-
-    //Verifica se o usuário existe e se a senha está correta
-    if ($user && password_verify($senha, $user['senha'])) {
         session_start();
         $_SESSION["usuario"] = $usuario;
         header("Location: painel.php"); // Redireciona para o painel
