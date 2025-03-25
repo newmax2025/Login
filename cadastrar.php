b3e9828c721a6e18d60597a9ae17c2ad784ac55f
newmax2025 <newmaxconsultas@gmail.com>
2025-03-24 22:45:56 -0300

    Update cadastrar.php

--- cadastrar.php
+++ cadastrar.php
@@ -4,10 +4,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $usuario = $_POST['username'];
     $senha = $_POST['password'];
 
+    $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
     $sql = "INSERT INTO clientes (usuario, senha) VALUES (:usuario, :senha)";
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':usuario', $usuario);
-    $stmt->bindParam(':senha', $senha);
+    $stmt->bindParam(':senha', $senha_hash);
     
     if ($stmt->execute()) {
         echo "Usuário cadastrado com sucesso!";
