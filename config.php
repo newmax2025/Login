68f47974442e66064a9994f9de7ca79fdcfac86e
newmax2025 <newmaxconsultas@gmail.com>
2025-03-24 22:08:22 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$host = "mysql.u377990636.hostinger.com";  // Substitua pelo seu host
+$host = "mysql.hostinger.com";  // Substitua pelo seu host
 $dbname = "u377990636_DataBase"; // Nome do banco de dados
 $username = "u377990636_Admin";   // Usuário do banco
 $password = "+c4Nrz@H5";         // Senha do banco
