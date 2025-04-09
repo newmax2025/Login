6743d5d6682e12d8c37e7b6bacb6789c81d41aeb
newmax2025 <newmaxconsultas@gmail.com>
2025-04-09 12:17:01 -0300

    Update aM.php

--- public/views/aM.php
+++ public/views/aM.php
@@ -153,10 +153,21 @@
             <img src="../assets/img/pesquisa_por_nome.jpg" alt="pesquisa_por_nome">
              <button class="botao">Sendo Adicionado</button>
                </div>
+
                <div class="card Gratuitos" onclick="mostrarBotao(this)">  
                 <img src="../assets/img/buscar_chassi.jpg" alt="pesquisa_por_nome">
                  <button class="botao">Sendo Adicionado</button>
                    </div>
+
+                   <div class="card Gratuitos" onclick="mostrarBotao(this)">  
+                    <img src="../assets/img/Foto_rj.jpg" alt="pesquisa_por_nome">
+                     <button class="botao">Sendo Adicionado</button>
+                       </div>
+
+                       <div class="card Gratuitos" onclick="mostrarBotao(this)">  
+                        <img src="../assets/img/Foto_sp.png" alt="pesquisa_por_nome">
+                         <button class="botao">Sendo Adicionado</button>
+                           </div>
     </div>
     </section>
     <section class="carousel2" id="favoritos">
